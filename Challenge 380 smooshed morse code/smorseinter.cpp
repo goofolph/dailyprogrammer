a4c6aa5f8aeb600e739cfda7a8579d2143e89c09
Caleb Branson <cbransonc14@gmail.com>
2019-09-23 15:31:10 -0400

    add intermediate source files

---
+++ Challenge 380 smooshed morse code/smorseinter.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "alphabet.h"
+#include "smorseinter.h"
+
+std::string smalpha(std::string input) { return ""; }
---
+++ Challenge 380 smooshed morse code/smorseinter.h
@@ -0,0 +1,5 @@
+#pragma once
+
+/* Given smooshed morse permutation of alphabet find possible original output.
+ */
+std::string smalpha(std::string input);
